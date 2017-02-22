b5f5609ec8f58d44465031b230d1e4a36f90fec4
Roberto Jimenez Sanchez <roberto@de.ibm.com>
2017-02-22 19:38:46 +0000

    Added boom formula

---
+++ boom.rb
@@ -0,0 +1,15 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Boom < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/jszroberto/boom/releases/download/0.1/boom"
+  sha256 "70f723af572fd57850a867d25bc3e3e4b774404f12adbc74f13a9b380bb984ac"
+
+  def install
+    bin.install boom
+  end
+
+end
