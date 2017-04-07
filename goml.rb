b21d59c2c99bca5aee803efac39062c3ff18da00
Julian Skupnjak <julian.skupnjak@gmail.com>
2017-04-07 13:45:46 +0200

    Update and rename boom.rb to goml.rb

--- goml.rb
+++ goml.rb
@@ -2,14 +2,14 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Boom < Formula
+class Goml < Formula
   desc ""
   homepage ""
-  url "https://github.com/jszroberto/boom/releases/download/0.1/boom"
-  sha256 "70f723af572fd57850a867d25bc3e3e4b774404f12adbc74f13a9b380bb984ac"
+  url "https://github.com/JulzDiverse/goml/releases/download/v0.1.0/goml-darwin-amd64"
+  sha256 "b6653d1c05a2254b3d1ab6146211227c669194b75d4ab91baa81a3ef979b8929"
 
   def install
-    bin.install "boom"
+    bin.install "goml"
   end
 
 end
