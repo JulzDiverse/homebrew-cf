083619911829c32fa9abbb8c788c6f4ded03aa49
Julian Skupnjak <julian.skupnjak@gmail.com>
2017-04-10 09:41:21 +0200

    Update goml.rb

--- goml.rb
+++ goml.rb
@@ -9,8 +9,8 @@ class Goml < Formula
   sha256 "b6653d1c05a2254b3d1ab6146211227c669194b75d4ab91baa81a3ef979b8929"
 
   def install
+    bin.install "goml-darwin-amd64"
     mv bin/"goml-darwin-amd64", bin/"goml"
-    bin.install "goml"
   end
 
 end
