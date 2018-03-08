34483e092e2006a323d4fdcd34d8da510d428cdc
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-03-08 10:24:21 +0100

    Update aviator.rb

--- aviator.rb
+++ aviator.rb
@@ -5,9 +5,9 @@
 class Aviator < Formula
   desc ""
   homepage ""
-  url "https://github.com/JulzDiverse/aviator/releases/download/v0.16.0/aviator-darwin-amd64"
-  sha256 "8b1e16938f9242a61a9e276d6cd463522256c0ae151636b605b319a60d82e9f1"
-  version "v0.16.0"
+  url "https://github.com/JulzDiverse/aviator/releases/download/v0.17.0/aviator-darwin-amd64"
+  sha256 "7fc23aa51e5acfa7b7a808329939f5eb04047a338886949215d9176bca802b91"
+  version "v0.17.0"
   
   def install
     FileUtils.mv("aviator-darwin-amd64", "aviator")
