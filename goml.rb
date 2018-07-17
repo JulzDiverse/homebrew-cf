b8f76e84dc8c00314209cd8f428d8974a4e26926
Julian Skupnjak <julian.skupnjak@gmail.com>
2018-07-17 16:23:05 +0200

    Update goml.rb

--- goml.rb
+++ goml.rb
@@ -5,9 +5,9 @@
 class Goml < Formula
   desc ""
   homepage ""
-  url "https://github.com/JulzDiverse/goml/releases/download/v0.4.0/goml-darwin-amd64"
-  sha256 "54d9ab644a16693cf55f84f55dadec14048f5f403f95f2816cd5f775df286ea6"
-  version "v0.4.0"
+  url "https://github.com/JulzDiverse/goml/releases/download/v0.5.0/goml-darwin-amd64"
+  sha256 "d11186a274336dbfa7b0588f1f47a005fb139cc8f9d696590b62b99004545789"
+  version "v0.5.0"
   
   def install
     FileUtils.mv("goml-darwin-amd64", "goml")
